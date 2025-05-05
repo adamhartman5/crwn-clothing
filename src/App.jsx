f43fb49d853de2018733e71e4a949f9b3a7778f6
Adam Hartman <adamhartman5@gmail.com>
2025-05-05 01:12:59 -0400

    Implement React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,12 @@
-import CategoryDirectory from './components/category-directory/category-directory';
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home/home';
 
 const App = () => {
-	const categories = [
-		{
-			id: 1,
-			title: 'Hats',
-			imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-		},
-		{
-			id: 2,
-			title: 'Jackets',
-			imageUrl: 'https://i.ibb.co/px2t6zj/jackets.png',
-		},
-		{
-			id: 3,
-			title: 'Sneakers',
-			imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-		},
-		{
-			id: 4,
-			title: 'Womens',
-			imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-		},
-		{
-			id: 5,
-			title: 'Mens',
-			imageUrl: 'https://i.ibb.co/R70vnyl/bags.png',
-		},
-	];
-	return <CategoryDirectory categories={categories} />;
+	return (
+		<Routes>
+			<Route path='/' element={<Home />} />
+		</Routes>
+	);
 };
 
 export default App;
