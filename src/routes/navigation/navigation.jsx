076c254a6917d23956073da86f6602f23b8fc513
Adam Hartman <adamhartman5@gmail.com>
2025-05-07 02:34:54 -0400

    Finish navigation bar and install firebase

---
+++ src/routes/navigation/navigation.jsx
@@ -0,0 +1,26 @@
+import { Fragment } from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import CrwnLogo from '../../assets/crown.svg?react';
+import './navigation.scss';
+
+const Navigation = () => {
+	return (
+		<>
+			<div className='navigation'>
+				<div className='logo-container'>
+					<Link className='logo' to='/'>
+						<CrwnLogo className='logo' />
+					</Link>
+				</div>
+				<div className='nav-links-container'>
+					<Link className='nav-link' to='/shop'>
+						Shop
+					</Link>
+				</div>
+			</div>
+			<Outlet />
+		</>
+	);
+};
+
+export default Navigation;
